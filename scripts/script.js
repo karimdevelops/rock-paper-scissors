9f881757fbc3cce0862d788292c4282d325a49cf
Abdul Karim <karim.develops@gmail.com>
2024-08-26 09:03:03 +0500

    Create callable code for choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
-let choices = ["Rock", "Paper", "Scissors"];
-let totalChoices = choices.length;
-let computerChoice = Math.floor(Math.random() * totalChoices);
+let computerChoice = getComputerChoice();
+
+function getComputerChoice() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let totalChoices = choices.length;
+  let randomChoice = Math.floor(Math.random() * totalChoices);
+  let choice = choices[randomChoice];
+  return choice;
+}
