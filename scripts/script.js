4cd6100abcd4887107f738e4cf0bcdf1dff2abc2
Abdul Karim <karim.develops@gmail.com>
2024-08-26 13:54:48 +0500

    Validate user\human choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,31 @@
+let choices = ["rock", "paper", "scissors"];
+let totalChoices = choices.length;
 let computerChoice = getComputerChoice();
-let choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-  let totalChoices = choices.length;
   let randomChoice = Math.floor(Math.random() * totalChoices);
   let choice = choices[randomChoice];
   return choice;
 }
 
 function getHumanChoice() {
-  let choice = prompt("Enter a choice (rock, paper, scissors)");
+  let choice = prompt("Enter a choice (rock, paper, scissors)").toLowerCase();
+  let choiceExists = checkChoice(choice);
+  while (!choiceExists) {
+    choice = prompt(
+      "Invalid choice!\nEnter a choice again (rock, paper, scissors).",
+    );
+    choiceExists = checkChoice(choice);
+  }
   return choice;
 }
+
+function checkChoice(choice) {
+  for (let i = 0; i < totalChoices; i++) {
+    if (choice == choices[i]) {
+      return true;
+    }
+  }
+}
+
+console.log(getHumanChoice());
